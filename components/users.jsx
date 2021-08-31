986a98e66b9caab904636b50bff2f3f4fc205f9e
sacden <sacden2009@gmail.com>
2021-08-31 23:42:33 +0200

    add pagination template

--- components/users.jsx
+++ components/users.jsx
@@ -1,13 +1,19 @@
 import React from "react";
-
+import Pagination from "./pagination";
 import User from "./user";
 
 const Users = ({ handleDelete, users }) => {
+  const count = users.length;
+  const pageSize = 4;
+  const handlePageChange = (pageIndex) => {
+    console.log("page:", pageIndex);
+  };
   return (
     <>
       {users.map((user) => (
         <User user={user} key={user._id} handleDelete={handleDelete} {...user} />
       ))}
+      <Pagination itemsCount={count} pageSize={pageSize} onPageChange={handlePageChange} />
     </>
   );
 };
